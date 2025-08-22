6ee3b2218b4b34ae07b10a6242bdb4857e651ba0
HannaPalianytsia <hannapalianytsia1@gmail.com>
2025-08-22 14:01:56 +0200

    initial commit

---
+++ src/components/header.js
@@ -0,0 +1,10 @@
+function Header() {
+  return `
+    <header>
+      <h1>ПОЇЗДКИ ТУРИ ПОДОРОЖІ</h1>
+    <img src="/travel.svg" alt="logo" class="logo" />
+    </header>
+  `;
+}
+
+export default Header;
---
+++ src/main.js
@@ -0,0 +1,4 @@
+import "./style.css";
+import Header from "./components/header";
+
+document.getElementById("header").innerHTML = Header();
