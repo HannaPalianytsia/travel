0e05a4a7269321af9b54560c6d76d7daa5685420
HannaPalianytsia <hannapalianytsia1@gmail.com>
2025-08-26 20:34:13 +0200

    add hero img

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,5 @@
 function Header() {
-  return `
-    <header>
-      <h1>ПОЇЗДКИ ТУРИ ПОДОРОЖІ</h1>
-    <img src="/travel.svg" alt="logo" class="logo" />
-    </header>
-  `;
+  return ``;
 }
 
 export default Header;
